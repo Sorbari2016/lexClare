c7ac6160a4bd321f9a86cb51afacc73071613a29
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-03-17 19:05:29 +0100

    Create the basic html the user sees when they visit

--- index.js
+++ index.js
@@ -6,11 +6,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 4000; 
+const PORT = 3000; 
 
 const API_KEY = process.env.SCH_DICT_API_KEY; 
 
+app.get("/", (req, res) => {
+    res.render("index.ejs"); 
+})
+
+
+app.get("/Dictionary", async (req, res) => {
+    try {
+        const word = req.query.word; 
+
+        if (!word || word.trim() === '') {
+            res.status(404).json({
+                message: "No word provided, please kindly provide a word"
+            })
+        }
+
+        const url = `https://www.dictionaryapi.com/api/v3/references/sd4/json/${word}?key=${API_KEY}`
+        const response = await axios.get(url); 
+
+    
+
+    } catch(error) {
+
+    }
+})
 
 app.listen(PORT, () => {
-    console.log(`API is running at http://localhost:${PORT}`)
+    console.log(`Server is running at http://localhost:${PORT}`)
 });
