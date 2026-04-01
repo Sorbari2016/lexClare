437e4f15f23c76fff3d69a1a699454332b59407c
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-04-01 20:36:04 +0100

    Create simplyResult method to get simple version of the result

--- index.js
+++ index.js
@@ -13,9 +13,36 @@ const API_KEY = process.env.SCH_DICT_API_KEY;
 // Use middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Use static files
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index.ejs"); 
-})
+}); 
+
+
+
+// Create method to get only data needed
+const simplifyResult = (data, query) => {
+    let escapedQuery = query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); 
+    let pattern = new RegExp(`^${escapedQuery}(:[a-z0-9]+)?$`, 'i'); 
+    
+    // filter the exact word searched
+    let filteredResult = data.filter(
+      (result) => pattern.test(result.meta.id)
+    ); 
+
+    // Get only properties needed for UI.
+    const head = filteredResult[0].hwi;
+    const headWord = head.hw.replace(/[^a-zA-Z]/g,'');
+    const phonetics = head.prs[0]?.mw || "N/A";
+    const sound = head.prs[0].sound.audio;
+    const definitions = filteredResult.map((definition) => {
+    return { pos: definition.fl, def: definition.shortdef };
+  });
+
+  return { hw: headWord, pho: phonetics, sound: sound, meaning: definitions };
+};
 
 
 app.post("/Search", async (req, res) => {
@@ -31,8 +58,10 @@ app.post("/Search", async (req, res) => {
         const url = `https://www.dictionaryapi.com/api/v3/references/sd4/json/${word}?key=${API_KEY}`
         const response = await axios.get(url); 
         const result = response.data; 
-        console.log(result);
-        res.render("index.ejs", {lexicon: result})
+        
+        const data = simplifyResult(result, word); 
+        console.log(data); 
+        res.render("index.ejs", {lexicon: data})
     } catch(error) {
         console.error("Failed to make request", error)
         res.render("index.ejs", {
