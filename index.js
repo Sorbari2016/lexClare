d4edf43695d4ab34f308f6f5b2f03d77096893bb
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-05-26 23:57:03 +0100

    Create a new file passport.js to place passport strategies in

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import pg from "pg";
 import session from "express-session";
+import passport from "passport";
 import methodOverride from "method-override";
 import multer from "multer"; // important for handle file uploads
 import bcrypt from "bcrypt"; // For hashing passwords securely
-import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
-import GoogleStrategy from "passport-google-oauth2";
-import { Strategy as FacebookStrategy } from "passport-facebook";
 import crypto from "crypto"; // to generate random token
-import nodemailer from "nodemailer";
+import nodemailer from "nodemailer"; // to send mail
 import path from "path";
 import { fileURLToPath } from "url";
 import ejs from "ejs";
+import "./passport.js";
 
 // Configure dotenv
 dotenv.config();
@@ -57,6 +55,13 @@ app.use(
   }),
 );
 
+// PASSPORT SETUP, after session
+// Initialize Passport authentication middleware
+app.use(passport.initialize());
+
+// Enable persistent login sessions
+app.use(passport.session());
+
 // Set transporter for nodemailer
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -65,13 +70,6 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
-
-// PASSPORT SETUP, after session
-// Initialize Passport authentication middleware
-app.use(passport.initialize());
-
-// Enable persistent login sessions
-app.use(passport.session());
 
 // Set up database
 const db = new pg.Client({
@@ -301,7 +299,7 @@ app.post("/recover", async (req, res) => {
     // create reset link
     const resetLink = `http://localhost:3000/recover/${resetToken}`;
 
-    // / send Email using EJS Template
+    // send Email using EJS Template
     const templatePath = path.join(__dirname, "/views/pages/mail.ejs");
 
     // create html
@@ -754,145 +752,8 @@ app.post("/search", async (req, res) => {
   }
 });
 
-// Register a passport local strategy
-passport.use(
-  new LocalStrategy(
-    { usernameField: "email" }, // Tell passport that instead of name='username' (as it expects), its email
-
-    async function verify(email, password, cb) {
-      try {
-        // Get user
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [
-          email,
-        ]);
-
-        // if user doesnt exist
-        if (result.rows.length === 0) {
-          return cb(null, false, {
-            message: "User do not exists, Try signing up",
-          });
-        }
-
-        const user = result.rows[0];
-
-        const isMatch = await bcrypt.compare(password, user.password); // verify user, compare passwords
-
-        // if user passes verification
-        if (isMatch) {
-          return cb(null, user);
-        } else {
-          return cb(null, false, {
-            message: "Incorrect password, try login in again",
-          });
-        }
-      } catch (err) {
-        return cb(err);
-      }
-    },
-  ),
-);
-
-// Set up a Google-based strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-
-    async (accessToken, refreshToken, profile, cb) => {
-      try {
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [
-          profile.email,
-        ]);
-        // if user does not exit
-        if (result.rows.length === 0) {
-          // destructure needed properties from google data
-          const {
-            name: { givenName, familyName },
-            email,
-          } = profile;
-          const newUser = await db.query(
-            `INSERT INTO users 
-             (first_name, last_name, email, password) 
-             VALUES ($1, $2, $3, $4)
-             RETURNING *`,
-            [givenName, familyName, email, "google"],
-          );
-          cb(null, newUser.rows[0]);
-        } else {
-          // Already have an existing user
-          const user = result.rows[0];
-          cb(null, user);
-        }
-      } catch (err) {
-        cb(err);
-      }
-    },
-  ),
-);
-
-// Set up Facebook Strategy
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3000/auth/facebook/callback",
-      profileFields: ["id", "displayName", "emails"],
-    },
-    async (accessToken, refreshToken, profile, cb) => {
-      try {
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [
-          profile?.emails[0]?.value,
-        ]);
-        // if user does not exit
-        if (result.rows.length === 0) {
-          // destructure needed properties from fb data
-          const {
-            displayName,
-            emails: [{ value: email }],
-          } = profile;
-          const [firstName, lastName] = displayName.split(" ");
-          const newUser = await db.query(
-            `INSERT INTO users 
-             (first_name, last_name, email, password) 
-             VALUES ($1, $2, $3, $4)
-             RETURNING *`,
-            [firstName, lastName, email, "facebook"],
-          );
-          cb(null, newUser.rows[0]);
-        } else {
-          // Already have an existing user
-          const user = result.rows[0];
-          cb(null, user);
-        }
-      } catch (err) {
-        cb(err);
-      }
-    },
-  ),
-);
-
-// Save user id to session cookie container
-passport.serializeUser((user, cb) => {
-  cb(null, user.id);
-});
-
-// Take the user ID out of the session and fetch full user obj from DB
-passport.deserializeUser(async (id, cb) => {
-  try {
-    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
-    const user = result.rows[0];
-
-    cb(null, user);
-  } catch (err) {
-    cb(err);
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+export { db };
